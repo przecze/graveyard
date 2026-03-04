5f425debd21464aea4dc8ff7c01025281b74fe66
Jan Czechowski <jan.czechowski@check24.de>
2026-03-04 21:38:51 +0100

    cleanup old frontend stuff

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import VisitorView from './VisitorView';
 import './index.css';
 
-const path = window.location.pathname.replace(/\/+$/, '') || '/';
-const RootComponent = path === '/visit' ? VisitorView : App;
-
-createRoot(document.getElementById('root')!).render(<RootComponent />);
+createRoot(document.getElementById('root')!).render(<VisitorView />);
